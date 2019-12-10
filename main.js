f037a33e740798eb214fe67547f92d14e7724c53
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-12-10 15:40:29 -0500

    complete

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
 class Vehicle {
+  constructor(name, wheelCount, maxSpeed) {
+    this.name = name;
+    this.wheelCount = wheelCount;
+    this.maxSpeed = maxSpeed;
+    this.speed = 0
+  }
+
+  accelerate () {
+    if (this.speed < this.maxSpeed) {
+      this.speed = this.speed + 1/3*this.maxSpeed
+    }
+  }
 }
 
-class Car {
+class Car extends Vehicle {
+  constructor(name,maxSpeed) {
+    super(name,4,maxSpeed) 
+
+
+  }
+
+  drive() {
+    return "Vroom. "
+  }
 }
 
-class Ferrari {
+class Ferrari extends Car {
+  constructor() {
+    super("Ferrari",180)
+  }
+
+  drive() {
+    return super.drive() + "I'm a Ferrari!"
+  }
+
+  accelerate () {
+    if (this.speed < this.maxSpeed) {
+      this.speed = this.speed + 1/2*this.maxSpeed
+    }
+  }
 }
 
 
@@ -13,3 +47,4 @@ module.exports = {
   Car,
   Ferrari
 }
+
